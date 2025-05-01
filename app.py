a28da003536424a20f3f9ed3fa541c1540548e38
nicoArriagadaj <nicolas.arriagada@ug.uchile.cl>
2025-05-01 05:34:19 -0400

    arreglar rutas segun buenas costumbres de programacion, ruta en minuscula, cambiando todos los htmls, y además, concentando el form con el def agregar_actividad por medio de  '/agregar_actividad'

--- app.py
+++ app.py
@@ -14,11 +14,11 @@ UPLOAD_FOLDER = 'static/fotos'
 def index():
     return render_template('index.html')
 
-@app.route('/AgregarActividad')
+@app.route('/agregar_actividad')
 def agregar():
     return render_template('AgregarActividad.html')
 
-@app.route('/Estadisticas')
+@app.route('/estadisticas')
 def estadisticas():
     return render_template('Estadisticas.html')
 
@@ -26,12 +26,12 @@ def estadisticas():
 def info():
     return render_template('infoOrdenada.html')
 
-@app.route('/ListadoDeActividades')
+@app.route('/listado_de_actividades')
 def listado():
     return render_template('ListadoDeActividades.html')
 
 
-@app.route('/AgregarActividad', methods=['POST'])
+@app.route('/agregar_actividad', methods=['POST'])
 def agregar_actividad():
     # procesar datos del formulario
     email = request.form['email']
